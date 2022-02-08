9f271b72d3e9874bb44536433a2563f93f170164
arathimetta <99226033+arathimetta@users.noreply.github.com>
2022-02-08 01:24:21 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,29 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import About from '@components/About';
+import Carrers from '@components/Careers';
+import Services from '@components/Services';
+import Contactus from '@components/Contactus';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>TEKSERVS</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header />
+        <About />
+        <Carrers />
+        <Services />
+        <Contactus />
       </main>
 
       <Footer />
