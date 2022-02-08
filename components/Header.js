9ddcb25664c91c94f5959a48b937a190e76cc05e
arathimetta <99226033+arathimetta@users.noreply.github.com>
2022-02-08 01:32:25 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,12 @@ export default function Header() {
   return (
     <>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-      <div class="navbar">
-        <a class="active" href="#"><i class="fa fa-fw fa-home"></i><span> Home</span></a>
-        <a href="#aboutus"><i class="fa fa-fw fa-address-card"></i> <span> About Us</span></a>
-        <a href="#careers"><i class="fa fa-fw fa-user-md"></i><span> Carrers</span></a>
-        <a href="#services"><i class="fa fa-fw fa-concierge-bell"></i> <span> Services</span></a>
-        <a href="#contactus"><i class="fa fa-fw fa-phone-laptop"></i><span> Contact Us</span></a>
+      <div className="navbar">
+        <a className="active" href="#"><i className="fa fa-fw fa-home"></i><span> Home</span></a>
+        <a href="#aboutus"><i className="fa fa-fw fa-address-card"></i> <span> About Us</span></a>
+        <a href="#careers"><i className="fa fa-fw fa-user-md"></i><span> Carrers</span></a>
+        <a href="#services"><i className="fa fa-fw fa-concierge-bell"></i> <span> Services</span></a>
+        <a href="#contactus"><i className="fa fa-fw fa-phone-laptop"></i><span> Contact Us</span></a>
       </div>
     </>
   );
