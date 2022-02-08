fe053d0f5de26d9b1963e5dd08d38edd25420b14
arathimetta <99226033+arathimetta@users.noreply.github.com>
2022-02-08 01:25:07 -0600

    Create Services.js

---
+++ components/Services.js
@@ -0,0 +1,46 @@
+export default function Services() {
+    return (
+        <div className="box-container" id="services">
+            <h1 className="text-center">Services</h1>
+            <img src='https://idctechnologies.com/images/services/banner.jpg' />
+            <p>At TEKSERVS, we understand the evolving business needs when it comes to IT. With our years of experience, we help you with bespoke solutions under managed IT services, IT staffing and recruiting.</p>
+            <ul>
+                <li>Workforce Solutions</li>
+                <li>Digital Services</li>
+                <li>Application Services</li>
+                <li>Managed Services</li>
+            </ul>
+            <div>
+                <h3>Cloud Solutions</h3>
+                <p><strong>Scale to the cloud, to scale up your business.</strong></p>
+                <p>From small, mid-level, to large, all businesses need to adapt to the latest technologies to stay ahead of their game. Cloud, as such, offers reliability, flexibility, and scalability to your business all along.</p>
+            </div>
+
+            <div>
+                <h3>Cybersecurity</h3>
+                <p><strong>Secure Data Space, and High-end Security for Cyberspace.</strong></p>
+                <p>Cybersecurity is an important part of any sort of data sharing process. With&nbsp;cyber-attacks on the rise, it's necessary for businesses to protect themselves with increased cybersecurity protocols.</p>
+            </div>
+            <div>
+                <h3>Data Analytic</h3>
+                <p><strong>Agile CMS solutions addressing the intricacies of Business Operations.</strong></p>
+                <p>Data is a flow of information from one to another, it can be structured or unstructured. Big data analytics is a science that gives ways to analyze, extract useful information from tons of data produced every day.</p>
+            </div>
+            <div>
+                <h3>Application Development</h3>
+                <p><strong>Develop efficient applications for your business.</strong></p>
+                <p>There is a need to develop applications which are easy to use, efficient as well as scalable. Businesses need to automate and simplify repetitive tasks to enable the desired operational growth.</p>
+            </div>
+            <div>
+                <h3>Contract Staffing & Workforce Solutions</h3>
+                <p><strong>Manpower Contract Staffing, Talent Based Outsourcing .</strong></p>
+                <p>Our comprehensive brand address the complex workforce challenges organizations face today, from contingent and permanent staffing to talent management, outsourcing, and talent development. We deliver the solutions that drive your business forward.</p>
+            </div>
+            <div>
+                <h3>ERP - Enterprise Resource Planning</h3>
+                <p><strong>Agile CMS solutions addressing the intricacies of Business Operations.</strong></p>
+                <p>Data is a flow of information from one to another, it can be structured or unstructured. Big data analytics is a science that gives ways to analyze, extract useful information from tons of data produced every day.</p>
+            </div>
+        </div>
+    );
+}
