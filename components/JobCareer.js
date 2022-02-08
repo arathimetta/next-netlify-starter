97a36418871b57e602dcd9b8e316e3801300b082
arathimetta <99226033+arathimetta@users.noreply.github.com>
2022-02-08 02:00:59 -0600

    Create JobCareer.js

---
+++ components/JobCareer.js
@@ -0,0 +1,11 @@
+export default function JobCareer() {
+    return (
+        <div className="box-container" id="careers">
+            <h1 className="text-center">Careers</h1>
+            <img src='https://www.autozone.com/cdn/images/B2B/Content/LandingPage/ProfessionalTraining/Tech_Image_d.jpg' />
+            <p>TEKSERVS is always looking for talented individuals to join our team! Our people make us who we are, and we recruit those who not only share our goals, but who are excited to be part of our responsive culture. We strongly encourage both a committed and motivated spirit as well as a balanced lifestyle allowing for personal growth and career development.</p>
+            <p>We're always looking for talented, passionate people. Browse our current listings to find the fit that's right for you.</p>
+            <p>Tell Us Where You Think You Fit Best, We'll Tell You How To Get There.</p>
+        </div>
+    );
+}
