ad1d47ebd8a550a824c17a21e1f8515f089d7608
Srutin Palla <srutin.kumar.reddy.palla@autozone.com>
2022-02-11 14:39:44 -0600

    update2

--- components/About.js
+++ components/About.js
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className="box-container" id="aboutus">
             <h1 className="text-center">ABOUT TEKSERVS</h1>
-            <img src='https://www.ars-it.com/assests/images/banner.jpg' style={{ width: "100%" }} />
+            <img src='https://www.ars-it.com/assests/images/banner.jpg' />
             <div className="margin32px">
                 <p>Here at TEKSERVS, our mission goes beyond providing technology solutions. We help our clients achieve the highest level of operational excellence so they can focus on what matters - their core business. Through our comprehensive range of services, we ensure our clients stay ahead in the ever-changing technology landscape and maximize their overall success.</p>
                 <p>We, at TEKSERVS, would like to your preferred partner and advisor in overcoming your recruitment challenges. The scale of benefits we can offer include the tangible and measurable value in providing quality talent that can meet your exacting needs.</p>
