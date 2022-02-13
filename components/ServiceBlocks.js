66a3cdda208238f361e3b40d0cbc7880fed91e8b
Srutin Palla <srutin.kumar.reddy.palla@autozone.com>
2022-02-12 21:50:23 -0600

    update6

--- components/ServiceBlocks.js
+++ components/ServiceBlocks.js
@@ -8,21 +8,21 @@ const ServiceBlocks = () => {
                     <div>
                         <h3>Cloud Solutions</h3>
                         <p><strong>Scale to the cloud, to scale up your business.</strong></p>
-                        <p>From small, mid-level, to large, all businesses need to adapt to the latest technologies to stay ahead of their game. Cloud, as such, offers reliability, flexibility, and scalability to your business all along.</p>
+                        <p className='text-justify'>From small, mid-level, to large, all businesses need to adapt to the latest technologies to stay ahead of their game. Cloud, as such, offers reliability, flexibility, and scalability to your business all along.</p>
                     </div>
                 </div>
                 <div className="col-sm-4 dataServiceBlocks">
                     <div>
                         <h3>Cybersecurity</h3>
                         <p><strong>Secure Data Space, and High-end Security for Cyberspace.</strong></p>
-                        <p>Cybersecurity is an important part of any sort of data sharing process. With&nbsp;cyber-attacks on the rise, it's necessary for businesses to protect themselves with increased cybersecurity protocols.</p>
+                        <p className='text-justify'>Cybersecurity is an important part of any sort of data sharing process. With&nbsp;cyber-attacks on the rise, it's necessary for businesses to protect themselves with increased cybersecurity protocols.</p>
                     </div>
                 </div>
                 <div className="col-sm-4 dataServiceBlocks">
                     <div>
                         <h3>Data Analytics</h3>
                         <p><strong>Agile CMS solutions addressing the intricacies of Business.</strong></p>
-                        <p>Data is a flow of information from one to another, it can be structured or unstructured. Big data analytics is a science that gives ways to analyze, extract useful info from tons of data produced every day.</p>
+                        <p className='text-justify'>Data is a flow of information from one to another, it can be structured or unstructured. Big data analytics is a science that gives ways to analyze, extract useful info from tons of data produced every day.</p>
                     </div>
                 </div>
             </div>
@@ -31,21 +31,21 @@ const ServiceBlocks = () => {
                     <div>
                         <h3>Application Development</h3>
                         <p><strong>Develop efficient applications for your business.</strong></p>
-                        <p>There is a need to develop applications which are easy to use, efficient as well as scalable in the current It industry. Businesses need to automate and simplify repetitive tasks to enable the desired operational growth.</p>
+                        <p className='text-justify'>There is a need to develop applications which are easy to use, efficient as well as scalable in the current It industry. Businesses need to automate and simplify repetitive tasks to enable the desired operational growth.</p>
                     </div>
                 </div>
                 <div className="col-sm-4 dataServiceBlocks">
                     <div>
                         <h3>Contract Staffing & Workforce Solutions</h3>
                         <p><strong>Manpower Contract Staffing, Talent Based Outsourcing .</strong></p>
-                        <p>Our comprehensive brand address the complex workforce challenges organizations face today, from contingent and permanent staffing to talent management and outsourcing.</p>
+                        <p className='text-justify'>Our comprehensive brand address the complex workforce challenges organizations face today, from contingent and permanent staffing to talent management and outsourcing.</p>
                     </div>
                 </div>
                 <div className="col-sm-4 dataServiceBlocks">
                     <div>
                         <h3>ERP - Enterprise Resource Planning</h3>
                         <p><strong>Agile CMS solutions addressing the intricacies of Business.</strong></p>
-                        <p>Data is a flow of information from one to another, it can be structured or unstructured. Big data analytics gives ways to analyze, extract useful information from tons of data produced every day.</p>
+                        <p className='text-justify'>Data is a flow of information from one to another, it can be structured or unstructured. Big data analytics gives ways to analyze, extract useful information from tons of data produced every day.</p>
                     </div>
                 </div>
             </div>
