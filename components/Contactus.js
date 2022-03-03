05808e660477db6c18907fc67a6f8a381f9c539f
Srutin Palla <srutin.kumar.reddy.palla@autozone.com>
2022-03-03 12:38:42 -0600

    emailjs update

--- components/Contactus.js
+++ components/Contactus.js
@@ -1,20 +1,57 @@
 import Button from '@mui/material/Button';
+import emailjs from '@emailjs/browser';
+import { useState } from 'react';
 
 export default function Contactus() {
     const styles = {
         "marginBottom": "16px"
     }
+    const [formSubmitted, setformSubmitted] = useState(false);
+    // const showContactusForm = () => {
+    //     setformSubmitted(false);
+    // }
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_6zqd6xl', 'template_dcf1urm', e.target, 'nPOCECiSLmM5i3Qls')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+        setformSubmitted(true)
+    }
     return (
-        <div className="box-container" id="contactus">
-            <h1 className="text-center">Contact Us</h1>
-            <img src='https://www.apexsystems.com/sites/default/files/media-images/ContactUs_Hero.jpg' alt="banner" />
-            <div className="margin32px">
-                <p>Feel Free to Contact us</p>
-                <input type="text" name="full_name" id="contact_full_name" placeholder="Full Name *" className="form-text required form-control" style={styles}></input>
-                <input type="text" name="phone_number" id="contact_phone_number" placeholder="Phone Number" className="form-text required form-control" style={styles}></input>
-                <textarea id="contact_message" name="message" rows="5" cols="60" placeholder="Message" className="form-textarea form-control resize-vertical" style={styles}></textarea>
-                <Button variant="contained">Submit</Button>
+        <>
+
+            <div className="box-container" id="contactus">
+                <h1 className="text-center">Contact Us</h1>
+                <img src='https://www.apexsystems.com/sites/default/files/media-images/ContactUs_Hero.jpg' alt="banner" />
+
+                {formSubmitted ? <>
+                    <div className="box-container" id="contactus">
+                        <div className="margin32px text-center">
+                            <h2 style={{ color: '#0095d3' }}>THANKS FOR REACHING OUT</h2>
+                            <h4>We'll be in touch soon.</h4>
+                            {/* <Button variant="contained" onClick={showContactusForm}>Submit</Button> */}
+                        </div>
+                    </div>
+                </> :
+                    <>
+                        <div className="margin32px">
+                            <p>Feel Free to Contact us</p>
+                            <form onSubmit={sendEmail}>
+                                <input type="text" name="full_name" id="contact_full_name" placeholder="Full Name*" className="form-text required form-control" style={styles} required></input>
+                                <input type="text" name="phone_number" id="contact_phone_number" placeholder="Phone Number*" className="form-text required form-control" style={styles} required></input>
+                                <input type="text" name="email" id="contact_email" placeholder="Email*" className="form-text required form-control" style={styles} required></input>
+                                <textarea id="contact_message" name="message" rows="5" cols="60" placeholder="Message*" className="form-textarea form-control resize-vertical" style={styles} required></textarea>
+                                <Button variant="contained" type="submit">Submit</Button>
+                            </form>
+                        </div>
+                    </>
+                }
             </div>
-        </div>
+
+        </>
     );
 }
